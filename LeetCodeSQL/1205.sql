83497dcdb25087c4bb2e740480c1acc6a370ad87
Miraz Uz Zaman <40580823+zamanmiraz@users.noreply.github.com>
2024-06-30 00:57:36 -0400

    Update 1205.sql

--- LeetCodeSQL/1205.sql
+++ LeetCodeSQL/1205.sql
@@ -12,27 +12,20 @@
 
 # Full Join = 1 UNION 2
 
--- Perform the left join part
-SELECT 
-    COALESCE(T1.month, T2.month) AS month,
-    COALESCE(T1.country, T2.country) AS country,
-    COALESCE(T1.approved_count, 0) AS approved_count,
-    COALESCE(T1.approved_amount, 0) AS approved_amount,
-    COALESCE(T2.chargeback_count, 0) AS chargeback_count,
-    COALESCE(T2.chargeback_amount, 0) AS chargeback_amount
-FROM
-    (SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(state) AS approved_count, SUM(amount) AS approved_amount
-     FROM Transactions
-     WHERE state = 'approved'
-     GROUP BY month, country) T1
-LEFT JOIN
-    (SELECT DATE_FORMAT(C.trans_date, '%Y-%m') AS month, country, COUNT(state) AS chargeback_count, SUM(amount) AS chargeback_amount
-     FROM Chargebacks C
-     JOIN Transactions T ON C.trans_id = T.id
-     GROUP BY month, country) T2
-ON T1.month = T2.month AND T1.country = T2.country
-
-UNION
+WITH T1 AS 
+    (
+        SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(state) AS approved_count, SUM(amount) AS approved_amount
+        FROM Transactions
+        WHERE state = 'approved'
+        GROUP BY month, country
+    ),
+T2 AS
+    (
+        SELECT DATE_FORMAT(C.trans_date, '%Y-%m') AS month, country, COUNT(state) AS chargeback_count, SUM(amount) AS chargeback_amount
+        FROM Chargebacks C
+        JOIN Transactions T ON C.trans_id = T.id
+        GROUP BY month, country
+    )
 
 -- Perform the right join part
 SELECT 
@@ -42,14 +35,19 @@ SELECT
     COALESCE(T1.approved_amount, 0) AS approved_amount,
     COALESCE(T2.chargeback_count, 0) AS chargeback_count,
     COALESCE(T2.chargeback_amount, 0) AS chargeback_amount
-FROM
-    (SELECT DATE_FORMAT(trans_date, '%Y-%m') AS month, country, COUNT(state) AS approved_count, SUM(amount) AS approved_amount
-     FROM Transactions
-     WHERE state = 'approved'
-     GROUP BY month, country) T1
-RIGHT JOIN
-    (SELECT DATE_FORMAT(C.trans_date, '%Y-%m') AS month, country, COUNT(state) AS chargeback_count, SUM(amount) AS chargeback_amount
-     FROM Chargebacks C
-     JOIN Transactions T ON C.trans_id = T.id
-     GROUP BY month, country) T2
+FROM T1 LEFT JOIN T2
+ON T1.month = T2.month AND T1.country = T2.country
+
+UNION
+
+-- Perform the right join part
+
+SELECT 
+    COALESCE(T1.month, T2.month) AS month,
+    COALESCE(T1.country, T2.country) AS country,
+    COALESCE(T1.approved_count, 0) AS approved_count,
+    COALESCE(T1.approved_amount, 0) AS approved_amount,
+    COALESCE(T2.chargeback_count, 0) AS chargeback_count,
+    COALESCE(T2.chargeback_amount, 0) AS chargeback_amount
+FROM T1 RIGHT JOIN T2
 ON T1.month = T2.month AND T1.country = T2.country
