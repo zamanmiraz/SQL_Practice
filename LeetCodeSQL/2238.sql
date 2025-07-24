b0c98900928963ddae56665164ae1221a52ba133
Miraz Uz Zaman <40580823+zamanmiraz@users.noreply.github.com>
2025-07-23 23:52:46 -0400

    Create 2238.sql

---
+++ LeetCodeSQL/2238.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH unique_driver AS
+    (
+        SELECT DISTINCT driver_id
+        FROM Rides
+    )
+
+SELECT r1.driver_id, COUNT(passenger_id) AS cnt
+FROM unique_driver r1 LEFT JOIN Rides r2 ON r1.driver_id = r2.passenger_id
+GROUP BY r1.driver_id
