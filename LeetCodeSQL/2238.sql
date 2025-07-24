43ed0ab58ea018055a457104c3587e6911869c0b
Miraz Uz Zaman <40580823+zamanmiraz@users.noreply.github.com>
2025-07-23 23:58:41 -0400

    Update 2238.sql

--- LeetCodeSQL/2238.sql
+++ LeetCodeSQL/2238.sql
@@ -8,3 +8,11 @@ WITH unique_driver AS
 SELECT r1.driver_id, COUNT(passenger_id) AS cnt
 FROM unique_driver r1 LEFT JOIN Rides r2 ON r1.driver_id = r2.passenger_id
 GROUP BY r1.driver_id
+
+
+
+% More optimized
+
+SELECT r1.driver_id, COUNT(DISTINCT r2.ride_id) cnt
+FROM Rides r1 LEFT JOIN Rides r2 ON r1.driver_id = r2.passenger_id
+GROUP BY r1.driver_id
